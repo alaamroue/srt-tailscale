aa629ddc706f4d14bee08cae5bb73f6c49eb4225
alaamroue <32003483+alaamroue@users.noreply.github.com>
2025-11-22 21:37:25 +0100

    update streamer

--- infra/srt-client/streamer.sh
+++ infra/srt-client/streamer.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -eu
 
-mjpg_streamer   -i "input_uvc.so -d /dev/video0 \
-                -r $CLIENT_STREAM_RESOLUTION \
-                -f 30" \
-                -o "output_http.so -w ./www -p 8080"
+exec ustreamer \
+	--device=/dev/video0 \
+	--host=0.0.0.0 \
+	--port=8080 \
+	--format=MJPEG \
+	-r $CLIENT_STREAM_RESOLUTION
