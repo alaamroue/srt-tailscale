19a162cd66b5a6da0410a45928eb3f073e5d5453
alaamroue <32003483+alaamroue@users.noreply.github.com>
2025-11-23 00:10:56 +0000

    Simpler setup

--- infra/srt-client/recorder.sh
+++ infra/srt-client/recorder.sh
@@ -8,7 +8,7 @@ ffmpeg \
   -c:v "$CLIENT_LOCAL_ENCODER" \
   -crf "$CLIENT_LOCAL_ENCODER_CRF" \
   -preset "$CLIENT_LOCAL_ENCODER_PRESET" \
-  -vf "fps=30,drawtext=fontfile=/usr/share/fonts/dejavu/DejaVuSans.ttf:text='%{localtime} UTC':x=W-tw-10:y=10:fontcolor=white:box=1:boxcolor=0x00000099" \
+  -vf "format=yuv420p,fps=30,drawtext=fontfile=/usr/share/fonts/dejavu/DejaVuSans.ttf:text='%{localtime} UTC':x=W-tw-10:y=10:fontcolor=white:box=1:boxcolor=0x00000099" \
   -hls_time 5 \
   -hls_list_size 17280 \
   -hls_segment_filename "/archive/${START_DATE}_%05d.ts" \
