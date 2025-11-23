39e08238321e7dcd6de663c41c7678c812ecb494
alaamroue <32003483+alaamroue@users.noreply.github.com>
2025-11-23 01:14:07 +0100

    feat: Update recorder script to handle different encoder configurations

--- infra/srt-client/recorder.sh
+++ infra/srt-client/recorder.sh
@@ -3,15 +3,29 @@ set -eu
 
 START_DATE=$(date +%Y_%m_%d-%H_%M_%S)
 
-ffmpeg \
-  -i "http://localhost:8080/?action=stream" \
-  -c:v "$CLIENT_LOCAL_ENCODER" \
-  -crf "$CLIENT_LOCAL_ENCODER_CRF" \
-  -preset "$CLIENT_LOCAL_ENCODER_PRESET" \
-  -vf "fps=30,drawtext=fontfile=/usr/share/fonts/dejavu/DejaVuSans.ttf:text='%{localtime} UTC':x=W-tw-10:y=10:fontcolor=white:box=1:boxcolor=0x00000099" \
-  -hls_time 5 \
-  -hls_list_size 17280 \
-  -hls_segment_filename "/archive/${START_DATE}_%05d.ts" \
-  -hls_flags program_date_time+append_list+omit_endlist+independent_segments+delete_segments \
-  -movflags +faststart \
-  "/archive/playlist.m3u8"
+if [ "$CLIENT_LOCAL_ENCODER" = "h264_v4l2m2m" ]; then
+	ffmpeg \
+		-i "http://localhost:8080/?action=stream" \
+		-c:v "$CLIENT_LOCAL_ENCODER" \
+    	-b:v 1500k -maxrate 1500k -bufsize 3000k \
+		-vf "format=yuv420p,fps=30,drawtext=fontfile=/usr/share/fonts/dejavu/DejaVuSans.ttf:text='%{localtime} UTC':x=W-tw-10:y=10:fontcolor=white:box=1:boxcolor=0x00000099" \
+		-hls_time 5 \
+		-hls_list_size 17280 \
+		-hls_segment_filename "/archive/${START_DATE}_%05d.ts" \
+		-hls_flags program_date_time+append_list+omit_endlist+independent_segments+delete_segments \
+		-movflags +faststart \
+		"/archive/playlist.m3u8"
+else
+	ffmpeg \
+		-i "http://localhost:8080/?action=stream" \
+		-c:v "$CLIENT_LOCAL_ENCODER" \
+		-crf "$CLIENT_LOCAL_ENCODER_CRF" \
+		-preset "$CLIENT_LOCAL_ENCODER_PRESET" \
+		-vf "fps=30,drawtext=fontfile=/usr/share/fonts/dejavu/DejaVuSans.ttf:text='%{localtime} UTC':x=W-tw-10:y=10:fontcolor=white:box=1:boxcolor=0x00000099" \
+		-hls_time 5 \
+		-hls_list_size 17280 \
+		-hls_segment_filename "/archive/${START_DATE}_%05d.ts" \
+		-hls_flags program_date_time+append_list+omit_endlist+independent_segments+delete_segments \
+		-movflags +faststart \
+		"/archive/playlist.m3u8"
+fi
