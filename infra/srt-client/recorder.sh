0eda7db8cb6e3e2392002c2d9714aef0e560a9d4
alaamroue <32003483+alaamroue@users.noreply.github.com>
2025-11-23 01:41:16 +0100

    feat: Refactor Docker and Nginx configurations for improved performance and maintainability

--- infra/srt-client/recorder.sh
+++ infra/srt-client/recorder.sh
@@ -6,8 +6,7 @@ START_DATE=$(date +%Y_%m_%d-%H_%M_%S)
 ffmpeg \
   -i "http://localhost:8080/?action=stream" \
   -c:v "$CLIENT_LOCAL_ENCODER" \
-  -crf "$CLIENT_LOCAL_ENCODER_CRF" \
-  -preset "$CLIENT_LOCAL_ENCODER_PRESET" \
+  -b:v 1500k -maxrate 1500k -bufsize 3000k \
   -vf "format=yuv420p,fps=30,drawtext=fontfile=/usr/share/fonts/dejavu/DejaVuSans.ttf:text='%{localtime} UTC':x=W-tw-10:y=10:fontcolor=white:box=1:boxcolor=0x00000099" \
   -hls_time 5 \
   -hls_list_size 17280 \
